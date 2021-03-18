5c40e2b2ad327aee3e80ee35bc375ec92b67736e
Matteo <matteomigliori280489@gmail.com>
2021-03-18 17:09:21 +0100

    primo push (da sistemare)

---
+++ VUE.JS/main.js
@@ -0,0 +1,14 @@
+var root = new Vue({
+    el: '#root',
+    data:{
+        message: 'SPERIAMO!!!',
+        img: 'https://www.borisitalia.it/blog/wp-content/uploads/2021/01/duccio-zona-bianca-1-768x560.jpg',
+        
+    }
+
+
+
+
+
+
+});
